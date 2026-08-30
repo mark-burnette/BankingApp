8e3d479872376483146265e0985bb0361b648d11
mark-burnette <burnette.mark@ufl.edu>
2026-08-30 16:50:07 -0400

    Added /users page with CRUD operations

--- bank-frontend/src/Users.tsx
+++ bank-frontend/src/Users.tsx
@@ -2,11 +2,18 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./Users.css";
 
-export interface UserModel {
+interface UserModel {
 	id: string;
 	name: string;
 	username: string;
 	role: string;
+}
+
+interface UpdateUserModel {
+	id: string;
+	name?: string;
+	username?: string;
+	role?: string;
 }
 
 async function fetchUsers(id: string | undefined) {
@@ -20,12 +27,48 @@ async function fetchUsers(id: string | undefined) {
 	return response.json();
 }
 
+async function updateUser(id: string, newUser: UpdateUserModel) {
+	const response = await fetch(`http://localhost:8000/users/${id}`, {
+		method: "PUT",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(newUser),
+	});
+
+	if (!response.ok) {
+		throw new Error(`HTTP Error: ${response.status}`);
+	}
+
+	return response.json();
+}
+
+async function deleteUser(id: string) {
+	const response = await fetch(`http://localhost:8000/users/${id}`, {
+		method: "DELETE",
+	});
+
+	if (!response.ok) {
+		throw new Error(`HTTP Error: ${response.status}`);
+	}
+}
+
 function Users() {
 	const [users, setUsers] = useState<UserModel[]>([]);
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState<string | null>(null);
 
 	const { id } = useParams<"id">();
+
+	function updateUserField(
+		id: string,
+		field: "name" | "username" | "role",
+		value: string,
+	) {
+		setUsers((currentUsers) =>
+			currentUsers.map((user) =>
+				user.id === id ? { ...user, [field]: value } : user,
+			),
+		);
+	}
 
 	useEffect(() => {
 		fetchUsers(id)
@@ -35,7 +78,7 @@ function Users() {
 			})
 			.catch((error) => setError(error.message))
 			.finally(() => setLoading(false));
-	}, []);
+	}, [id]);
 
 	if (loading) {
 		return <p>Loading users...</p>;
@@ -56,14 +99,55 @@ function Users() {
 							<div className="avatar">{user.name.charAt(0).toUpperCase()}</div>
 
 							<div>
-								<div className="user-name">{user.name}</div>
-								<div className="username">@{user.username}</div>
+								<input
+									className="user-name"
+									value={user.name}
+									onChange={(e) =>
+										updateUserField(user.id, "name", e.target.value)
+									}
+								/>
+								<input
+									className="user-username"
+									value={user.username}
+									onChange={(e) =>
+										updateUserField(user.id, "username", e.target.value)
+									}
+								/>
 							</div>
+							<select
+								className="user-role"
+								value={user.role}
+								onChange={(e) =>
+									updateUserField(user.id, "role", e.target.value)
+								}
+							>
+								<option value="customer">customer</option>
+								<option value="admin">admin</option>
+							</select>
+							<div className="user-id">{user.id}</div>
 						</div>
 
-						<div className="user-role">{user.role}</div>
+						<button
+							className="update-button"
+							onClick={() => updateUser(user.id, user)}
+						>
+							Update
+						</button>
 
-						<div className="user-id">{user.id}</div>
+						<button
+							className="delete-button"
+							onClick={() =>
+								deleteUser(user.id).then(() => {
+									setUsers((currentUsers) =>
+										currentUsers.filter(
+											(currentUser) => currentUser.id !== user.id,
+										),
+									);
+								})
+							}
+						>
+							Delete
+						</button>
 					</div>
 				))}
 			</div>
